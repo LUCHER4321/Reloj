b0b8d62f123be0bf709c53862cec14d4760111b0
LUCHER4321 <lucher4321@gmail.com>
2025-03-01 20:04:14 -0300

    Create main.ts

---
+++ src/main.ts
@@ -0,0 +1,36 @@
+let dateTime = new Date();
+
+const getLabel = (id: string) => document.getElementById(id) as HTMLLabelElement;
+
+const units = ["day", "month", "year", "hour", "minute", "second"];
+const getFromDate: ((d: Date) => number)[] = [
+    (d) => d.getDay(),
+    (d) => d.getMonth(),
+    (d) => d.getFullYear(),
+    (d) => d.getHours(),
+    (d) => d.getMinutes(),
+    (d) => d.getSeconds()
+];
+const labels = units.map(u => getLabel(u));
+const unitLabelMap = new Map<string, HTMLLabelElement>();
+const unitGetMap = new Map<string, (d: Date) => number>();
+units.forEach((unit, index) => {
+    unitLabelMap.set(unit, labels[index]);
+    unitGetMap.set(unit, getFromDate[index]);
+});
+
+const setUnitLabel = (unit: string, date: Date) => {
+    const label = unitLabelMap.get(unit);
+    const value = unitGetMap.get(unit)?.(date);
+    const zero = value && (value < 10 ? "0" : "");
+    if(label) {
+        label.textContent = value ? zero + value.toString() : "00";
+    }
+}
+
+setInterval(() => {
+    dateTime = new Date();
+    for(const unit of units) {
+        setUnitLabel(unit, dateTime);
+    }
+}, 500);
